b4e49463293be866a6adb7c67d50d181d49197a6
janany23 <janany.perinparajah@ynov.com>
2017-01-04 16:56:22 +0100

    new project Symfony :(

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ydays
+project
+=======
+
+A Symfony project created on January 4, 2017, 3:32 pm.
--- app/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Home page
-$app->get('/', function () {
-    ob_start();             // start buffering HTML output
-    require '../views/views.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
-});
