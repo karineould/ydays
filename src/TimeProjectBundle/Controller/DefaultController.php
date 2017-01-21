d6e3a0c2353dfe4adf4231393cc827e0f5181038
Markoboby <mark.malaj@ynov.com>
2017-01-21 18:53:11 +0100

    Build creation society page

--- src/TimeProjectBundle/Controller/DefaultController.php
+++ src/TimeProjectBundle/Controller/DefaultController.php
@@ -14,13 +14,15 @@ class DefaultController extends Controller
         $em = $this->getDoctrine()->getManager();
         $test = $em->getRepository('OCUserBundle:User')->findExempleJan();
         
-        dump($test[0]->getUsername());
-        
         return $this->render('TimeProjectBundle:Default:index.html.twig', ['missions' => [] ]);
     }
 
     public function loginAction()
     {
         return $this->render('TimeProjectBundle:Default:login.html.twig');
+    }
+
+    public function createSocietyAction(){
+        return $this->render('TimeProjectBundle:Default:societyConf.html.twig');
     }
 }
